ddbf70212ddfceb7d0a582aabcf356e36e36e038
lip ables <phillipables1@gmail.com>
2018-11-21 21:37:50 -0500

    scripts added to game objects

--- standard-assets-pack/Assets/Scripts/PlayerScript.cs
+++ standard-assets-pack/Assets/Scripts/PlayerScript.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerScript : MonoBehaviour {
+    // we need some sort of currency and a display text to display it
+    public int money;
 
 	// Use this for initialization
 	void Start () {
-		
+        money = 150;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+  
 }
