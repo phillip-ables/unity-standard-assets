f1113e712f44d19531a02af39af31a6bc395edec
lip ables <phillipables1@gmail.com>
2018-11-21 21:44:38 -0500

    player script has money value, building script get player script on start

--- standard-assets-pack/Assets/Scripts/BuildingScript.cs
+++ standard-assets-pack/Assets/Scripts/BuildingScript.cs
@@ -3,14 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BuildingScript : MonoBehaviour {
+    private PlayerScript player;
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+        //player = GameObject.FindGameObjectWithTag("Player"); 
+        player.GetComponent<PlayerScript>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    public void GetBuilding() 
+    {
+
+    }
+
+    public void BuildBuilding()  // this will eventually take a player type
+    {
+        
+    }
 }
