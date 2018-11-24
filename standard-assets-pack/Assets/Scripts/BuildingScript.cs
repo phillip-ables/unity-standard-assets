fc6218ac9de9d884b8b28b6997dca647d3a05e71
lip ables <phillipables1@gmail.com>
2018-11-24 16:16:20 -0500

    alot of code, replacing jump for build, alot of it i dont think will be in the build script and should be in the fp controller script

--- standard-assets-pack/Assets/Scripts/BuildingScript.cs
+++ standard-assets-pack/Assets/Scripts/BuildingScript.cs
@@ -2,17 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
+using UnityStandardAssets.Utility;
 
 public class BuildingScript : MonoBehaviour {
     public GameObject cactusPrefab;
     public float distanceToBuild = 2.0f;
 
-    private bool m_Build;
-    private bool m_Building;
     private Transform playerTransform;
     private PlayerScript player;
     private Vector3 wallPos;
 
+    //okay so these are an exact replica of all jump vars
+    [SerializeField] private float m_BuildSpeed;
+    [SerializeField] private LerpControlledBob m_BuildBob = new LerpControlledBob();
+    [SerializeField] private AudioClip m_BuildSound;
+
+    private bool m_Build;
+
+    //this one isnt building but its used in the jumping unity code soooo
+    private Vector3 m_BuildDir = Vector3.zero;
+    private CharacterController m_CharacterController;
+    private bool m_PreviouslyBuilding;
+    private bool m_Building;
+    private AudioSource m_AudioSource;
 
 	// Use this for initialization
 	void Start () {
@@ -27,8 +39,16 @@ public class BuildingScript : MonoBehaviour {
         if (!m_Build)
         {
             m_Build = CrossPlatformInputManager.GetButtonDown("Build");
+            Debug.Log("m_Build is now true, you ready to build i think, or are you building?");
         }
 
+        if(!m_PreviouslyBuilding && m_CharacterController.isGrounded)
+        {
+            StartCoroutine(m_BuildBob.DoBobCycle());
+            PlayBuildingSound();
+            m_BuildDir.y = 0f;
+            m_Building = false;
+        }
 
         /*
         if (!m_Build)  // so this  should be because you cant double tap and build more or maybe you can idk
@@ -47,7 +67,55 @@ public class BuildingScript : MonoBehaviour {
         */
 	}
 
- 
+    private void PlayBuildingSound()
+    {
+        m_AudioSource.clip = m_BuildSound;
+        m_AudioSource.Play();
+        //i chose to keep this as a step because building is going to mess up your next step, there will be offset
+        m_NextStep = m_StepCycle + .5f;  // this may go in the fp controller when the function is called or i may just access the script from in here. 
+        //def if its already accessed then imma use it there too
+    }
+
+    private void FixedUpdate()
+    {
+        float speed;
+        GetInput(out speed){  // you have to access the first person controller
+            Vector3 desiredMove = transform.forward * m_moveInput.y + transform.right * m_moveInput.x;
+            //im pretty sure most of this stuff will go in the fp controller script
+
+            RaycastHit hitInfo;
+            Physics.SphereCast(transform.position, m_CharacterController.radius, Vector3.down, out hitInfo,
+                m_CharacterController.height / 2f, Physics.AllLayers, QueryTriggerInteraction.Ignore);
+
+            desiredMove = Vector3.ProjectOnPlane(desiredMove, hitInfo.normal).normalized;
+
+            m_MoveDir.x = desiredMove.x * speed;
+            m_MoveDir.y = desiredMove.z * speed;
+
+            if (m_CharacterController.isBuilding)
+            {
+                m_MoveDir.y = -m_StickToBuildingForce;
+
+                if (m_Build)
+                {
+                    m_MpveDir.y = m_BuildSpeed;
+                    PlayBuildSound();
+                    m_Build = false;
+                    m_Building = false;
+                }
+            }
+            else
+            {
+                m_MoveDir += Physics.gravity * m_GravityMultiplier * Time.fixedDeltaTime;
+            }
+            m_CollisionFlags = m_CharacterController.Move(m_MoveDir * Time.fixedDeltaTime);
+
+            ProgressStepCycle(speed);
+            UpdateCameraPosition(speed);
+        }
+    }
+
+
 
     public void BuildBuilding()  // this will eventually take a player type
     {
