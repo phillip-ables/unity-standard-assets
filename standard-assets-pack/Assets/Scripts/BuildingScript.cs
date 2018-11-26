ea5935e6b91db8df1c4a1d886ea8f6337486cf9c
lip ables <phillipables1@gmail.com>
2018-11-26 08:04:02 -0500

    building script cut down and hopefully bear bones usable

--- standard-assets-pack/Assets/Scripts/BuildingScript.cs
+++ standard-assets-pack/Assets/Scripts/BuildingScript.cs
@@ -14,15 +14,16 @@ public class BuildingScript : MonoBehaviour {
 
     //okay so these are an exact replica of all jump vars
     [SerializeField] private float m_BuildSpeed;
-    [SerializeField] private LerpControlledBob m_BuildBob = new LerpControlledBob();
-    [SerializeField] private AudioClip m_BuildSound;
+    [SerializeField] private LerpControlledBob m_BuildBob = new LerpControlledBob();  // Don't think this is an official unity type
+    [SerializeField] private AudioClip m_BuildSound;  // need this
+    //that will be a quick hammer along with a saw mill sound over the back of it
 
     private bool m_Build;
 
     //this one isnt building but its used in the jumping unity code soooo
     private Vector3 m_BuildDir = Vector3.zero;
-    private CharacterController m_CharacterController;
-    private bool m_PreviouslyBuilding;
+    private CharacterController m_CharacterController;  // this will probably  be left up to the fps controller
+    private bool m_PreviouslyBuilding;  // not sure if i need this or not
     private bool m_Building;
     private AudioSource m_AudioSource;
 
@@ -42,6 +43,7 @@ public class BuildingScript : MonoBehaviour {
             Debug.Log("m_Build is now true, you ready to build i think, or are you building?");
         }
 
+        // NOT JUMPING
         if(!m_PreviouslyBuilding && m_CharacterController.isGrounded)
         {
             StartCoroutine(m_BuildBob.DoBobCycle());
@@ -49,6 +51,7 @@ public class BuildingScript : MonoBehaviour {
             m_BuildDir.y = 0f;
             m_Building = false;
         }
+        // i think these two go in the main script and call all the inners of the second 
 
         /*
         if (!m_Build)  // so this  should be because you cant double tap and build more or maybe you can idk
@@ -67,53 +70,74 @@ public class BuildingScript : MonoBehaviour {
         */
 	}
 
-    private void PlayBuildingSound()
+    private void PlayBuildingSound()  // and see the audio source is on the main player so.... prob in fps script
     {
         m_AudioSource.clip = m_BuildSound;
         m_AudioSource.Play();
+
+        /*
+         * TEMPERARLY REMOVE THIS
+         * 
         //i chose to keep this as a step because building is going to mess up your next step, there will be offset
         m_NextStep = m_StepCycle + .5f;  // this may go in the fp controller when the function is called or i may just access the script from in here. 
         //def if its already accessed then imma use it there too
+
+        */
     }
 
+
+    /*
+     *  THIS WHOLE THING CAN GO TOO
+     *  it mostly handles the charachter position and finding out if there is a collision with your sphere
     private void FixedUpdate()
     {
         float speed;
-        GetInput(out speed){  // you have to access the first person controller
-            Vector3 desiredMove = transform.forward * m_moveInput.y + transform.right * m_moveInput.x;
-            //im pretty sure most of this stuff will go in the fp controller script
+        GetInput(out speed); // you have to access the first person controller
+        Vector3 desiredMove = transform.forward * m_moveInput.y + transform.right * m_moveInput.x;
+        //im pretty sure most of this stuff will go in the fp controller script
 
-            RaycastHit hitInfo;
-            Physics.SphereCast(transform.position, m_CharacterController.radius, Vector3.down, out hitInfo,
-                m_CharacterController.height / 2f, Physics.AllLayers, QueryTriggerInteraction.Ignore);
+        RaycastHit hitInfo;
+        Physics.SphereCast(transform.position, m_CharacterController.radius, Vector3.down, out hitInfo,
+            m_CharacterController.height / 2f, Physics.AllLayers, QueryTriggerInteraction.Ignore);
 
-            desiredMove = Vector3.ProjectOnPlane(desiredMove, hitInfo.normal).normalized;
+        desiredMove = Vector3.ProjectOnPlane(desiredMove, hitInfo.normal).normalized;
 
-            m_MoveDir.x = desiredMove.x * speed;
-            m_MoveDir.y = desiredMove.z * speed;
+        m_MoveDir.x = desiredMove.x * speed;
+        m_MoveDir.y = desiredMove.z * speed;
 
-            if (m_CharacterController.isBuilding)
+        if (m_CharacterController.isBuilding)
+        {
+            m_MoveDir.y = -m_StickToBuildingForce;
+
+            if (m_Build)
             {
-                m_MoveDir.y = -m_StickToBuildingForce;
-
-                if (m_Build)
-                {
-                    m_MpveDir.y = m_BuildSpeed;
-                    PlayBuildSound();
-                    m_Build = false;
-                    m_Building = false;
-                }
+                m_MpveDir.y = m_BuildSpeed;
+                PlayBuildSound();
+                m_Build = false;
+                m_Building = false;
             }
-            else
-            {
-                m_MoveDir += Physics.gravity * m_GravityMultiplier * Time.fixedDeltaTime;
-            }
-            m_CollisionFlags = m_CharacterController.Move(m_MoveDir * Time.fixedDeltaTime);
-
-            ProgressStepCycle(speed);
-            UpdateCameraPosition(speed);
         }
+        else
+        {
+            m_MoveDir += Physics.gravity * m_GravityMultiplier * Time.fixedDeltaTime;
+        }
+        m_CollisionFlags = m_CharacterController.Move(m_MoveDir * Time.fixedDeltaTime);
+
+        ProgressStepCycle(speed);
+        UpdateCameraPosition(speed);
+        
     }
+    */
+
+   
+    /*
+     * ALREADY HAVE A playbuildingsound
+    private void PlayBuildSound()
+    {
+        m_AudioSource.clip = m_BuildSound;
+        m_AudioSource.Play();
+    }
+    */
 
 
 
