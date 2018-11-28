578234068b537ff0f5a9d95a690188b479fde137
lip ables <phillipables1@gmail.com>
2018-11-28 12:10:23 -0500

    get dusty legs function to instantiate the rat at for each child of feet empty game object posiition at horses rotation, bool for all horses state and is two leg set to true on start

--- standard-assets-pack/Assets/Scripts/HorseScript.cs
+++ standard-assets-pack/Assets/Scripts/HorseScript.cs
@@ -3,14 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class HorseScript : MonoBehaviour {
+    public Transform feet;
+    public GameObject rabbit;
+    public GameObject dusty;
+
+    private bool isTwoLeg;
+    private bool isFourLeg;
+    private bool isBunnyLeg;
+    private bool isRatLeg;
 
 	// Use this for initialization
 	void Start () {
-		
+        isTwoLeg = true;
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // i dont think we need an update function
+    private void getDustyLegs()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            Instantiate(dusty, feet.GetChild(i).transform.position, transform.rotation);
+        }
+    }
+
+    public void ChangePosition()
+    {
+        // im gonna do a switch here
+    }
 }
