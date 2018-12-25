48726503dafbc9e88778813e8ccdf7bc3ab4be89
lip ables <phillipables1@gmail.com>
2018-12-25 02:19:07 -0500

    feed script redone, on start set feed speed int to 0, get animator for horse, if feed button pressed check if they can afford the feed cost, then take the feed cost set the trigger for the horse animator, if random range zero to two is greater then one increment feed speed, finallly set interger of horse animator to feed speed and set button back down is false is pressed

--- standard-assets-pack/Assets/Scripts/FeedScript.cs
+++ standard-assets-pack/Assets/Scripts/FeedScript.cs
@@ -4,18 +4,22 @@ using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
 
 public class FeedScript : MonoBehaviour {
-    public HorseScript horseScript;
-
     private bool m_Feed;
+    private Animator horse;
     private PlayerScript playerScript;
     private int feedCost = 20;
-    private int lastPosLvl;
-    private int currentBag = 0;
+    //decided to make it cost more and make it one shot,
+    //so if successful then you got it, if not then you dont
+    //private int lastPosLvl;
+    //private int currentBag = 0;
+    private int feedSpeedInt;
 
     // Use this for initialization
     void Start () {
         //Debug.Log("feed script present");
         playerScript = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerScript>();
+        feedSpeedInt = 0;
+        horse = GameObject.FindGameObjectWithTag("Horse").GetComponent<Animator>();
     }
 	
 	// Update is called once per frame
@@ -23,20 +27,30 @@ public class FeedScript : MonoBehaviour {
         if (!m_Feed)
         {
             m_Feed = CrossPlatformInputManager.GetButtonDown("Feed");
+            Debug.Log("Button is now ready");
         }
         else
         {
             if (playerScript.money > feedCost)
             {
-                //Debug.Log("feeding time!");
-                GetAnimal();
+                playerScript.money -= feedCost;
+                horse.SetTrigger("FeedPressed");
+                //playerScript.feed();
+                if(Random.Range(0,2) >= 1)
+                {
+                    feedSpeedInt++;
+                    //playerScript.feedSpeedInt++;
+                }
+                Debug.Log("level " + feedSpeedInt);
+                horse.SetInteger("FeedSpeedInt", feedSpeedInt);
+
             }
+            m_Feed = false;
         }
 	}
 
-    public void GetAnimal()
+    public void CatchAnimal()
     {
-        playerScript.money -= feedCost;
         //call animation for furry animal to mistily appear timidly look then
         int randomInt = Random.Range(0, 2);
         Debug.Log(randomInt);
