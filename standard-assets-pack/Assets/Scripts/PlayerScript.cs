f0d928148e1b40f079a68a3da52632bc22a105ca
lip ables <phillipables1@gmail.com>
2018-12-25 03:10:30 -0500

    private Animator on start get component animator, tw public methods one for setting trigger the other takes a int and sets the animator int, this is all in the player script

--- standard-assets-pack/Assets/Scripts/PlayerScript.cs
+++ standard-assets-pack/Assets/Scripts/PlayerScript.cs
@@ -5,19 +5,30 @@ using UnityEngine.UI;
 
 public class PlayerScript : MonoBehaviour {
     // we need some sort of currency and a display text to display it
-    public Text scoreText; 
+    public Text scoreText;
     public int money;
     public GameObject buildingPrefab;
     public GameObject fencePrefab;
 
-	// Use this for initialization
-	void Start () {
+    private Animator m_Animator;
+
+    // Use this for initialization
+    void Start() {
         money = 1000;
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
+
+    // Update is called once per frame
+    void Update() {
         scoreText.text = money.ToString();
-	}
-  
+    }
+
+    public void FeedPressed()
+    {
+        m_Animator.SetTrigger("FeedPressed");
+    }
+
+    public void SetInteger(int feedSpeedInt)
+    {
+        m_Animator.SetInteger("FeedSpeedInt", feedSpeedInt);
+    }
 }
